d318cc6c5eb438f63df9437925018ab2c25a6da7
Kristennorth <117308715+K10North@users.noreply.github.com>
2023-11-07 11:45:56 -0800

    Update Actor.h
    added class skeletons from the iceman packet

--- IceMan/Actor.h
+++ IceMan/Actor.h
@@ -4,5 +4,73 @@
 #include "GraphObject.h"
 
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class Actor: virtual public GraphObject {
+public:
+	int getX() const{ return 0;}
+	int getY() const{return 0;}
+    	virtual void doSomething(){
+        	//	void Ask_all_actors_to_do_something()
+        	//	{
+        	//	for each actor on the level:
+        	//	if (the actor is still alive)
+        	//	tell the actor to doSomething();
+        	//	}
+    	}
+    	virtual ~Actor() {}
+  //  …
+};
+class Protester: virtual public Actor {
+public:
+//setVisible(bool shouldIDisplay);
+void moveTo(int x, int y) {}
+};
+class HardcoreProtester: virtual public Protester {
+public:
+//…
+};
+
+class Prop : virtual public Actor{
+private:
+    
+public:
+};
+
+class IceMan : virtual public Actor{
+private:
+	int lives;
+public:
+	virtual bool isAlive(int lives){
+    	if (lives > 0){
+        	return true;
+    	}
+    	else{
+        	return false;
+    	}
+//    	void Iceman::doSomething()
+//    	{
+//    	...
+//    	int ch;
+//    	if (getWorld()->getKey(ch) == true)
+//    	{
+//    	// user hit a key this tick!
+//    	switch (ch)
+//    	{
+//    	case KEY_PRESS_LEFT:
+//    	... move player to the left ...;
+//    	break;
+//    	case KEY_PRESS_RIGHT:
+//    	... move player to the right ...;
+//    	break;
+//    	case KEY_PRESS_SPACE:
+//    	... add a Squirt in front of the player...;
+//    	break;
+//    	// etc…
+//    	}
+//    	}
+//    	...
+//    	}
+
+	}
+};
 
 #endif // ACTOR_H_
