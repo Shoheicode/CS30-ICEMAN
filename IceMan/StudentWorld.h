ac356d4d420e304a1846b89e6006025d17257f4b
Kristennorth <117308715+K10North@users.noreply.github.com>
2023-11-29 19:30:18 -0800

    gold and oil

--- IceMan/StudentWorld.h
+++ IceMan/StudentWorld.h
@@ -6,7 +6,7 @@
 #include "Actor.h"
 #include <string>
 #include <list>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <vector>
 #include <cmath>
 using namespace std;
@@ -21,21 +21,22 @@ public:
     {
     }
 
-	~StudentWorld() {
-		for (int i = 0; i <= 59; i++) {
-			for (int j = 0; j <= 64; j++) {
-				delete iceMap.at(i).at(j); //deletes every ice in vector;
-			}
-		}
-		iceMap.clear();
-		//Delete all the actors in character list
-		for (Actor* a : characterList) {
-			Actor* temp = a;
-			a = nullptr;
-			delete temp;
-		}
-		characterList.clear();
-	}
+    ~StudentWorld() {
+        for (int i = 0; i <= 59; i++) {
+            for (int j = 0; j <= 64; j++) {
+                delete iceMap.at(i).at(j); //deletes every ice in vector;
+            }
+        }
+        iceMap.clear();
+        //Delete all the actors in character list
+        for (Actor* a : characterList) {
+            Actor* temp = a;
+            a = nullptr;
+            delete temp;
+        }
+        characterList.clear();
+
+    }
 
     /*
     init() method must:
@@ -50,32 +51,35 @@ public:
     */
 
     virtual int init()
-    {   
-		//Number of Boulders
-		int bNum = min(static_cast<int>(getLevel())/ 2 + 2, 9);
+    {
+        //Number of Boulders
+        int bNum = min(static_cast<int>(getLevel())/ 2 + 2, 9);
 
-		//Number of Gold Nuggets
-		int gNum = max(5 - static_cast<int>(getLevel()) / 2, 2);
+        //Number of Gold Nuggets
+        int gNum = max(5 - static_cast<int>(getLevel()) / 2, 2);
 
-		//Number of Barrels of Oil
-		int oNum = min(static_cast<int>(getLevel()) + 2, 21);
+        //Number of Barrels of Oil
+        int oNum = min(static_cast<int>(getLevel()) + 2, 21);
 
-		srand(time(NULL));
+        srand(time(NULL));
 
-		//Creates the player
-		player = new IceMan(30, 60, this);
-		characterList.push_back(player);
+        //Creates the player
+        player = new IceMan(30, 60, this);
+        characterList.push_back(player);
 
-		//Create Ice
-		createIceMap();
+        //Create Ice
+        createIceMap();
 
-		//Spawns Boulders
-		spawnBoulders(bNum);
-		
-		//Spawns Nuggets
-		spawnNuggets(gNum);
+        //Spawns Boulders
+        spawnBoulders(bNum);
+        
+        //Spawns Nuggets
+        spawnNuggets(gNum);
+        
+        //Spawns Oil
+        spawnOil(oNum);
 
-		//Adds a protestor
+        //Adds a protestor
         characterList.push_back(new Protester(60, 60, this));
 
 
@@ -101,46 +105,48 @@ public:
     
     */
 
-	virtual int move()
-	{
-		// This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
-		// Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
-		// cout << iceMap.size() << endl;
-		
-		//Updates the Textbox
-		updateTextBox();
+    virtual int move()
+    {
+        // This code is here merely to allow the game to build, run, and terminate after you hit enter a few times.
+        // Notice that the return value GWSTATUS_PLAYER_DIED will cause our framework to end the current level.
+        // cout << iceMap.size() << endl;
+        
+        //Updates the Textbox
+        updateTextBox();
 
-		//Goes through each character and asks if it does something
-		for (Actor* a : characterList) {
-			if (a->isAlive()) {
-				a->doSomething();
+        //Goes through each character and asks if it does something
+        for (Actor* a : characterList) {
+            if (a->isAlive()) {
+                a->doSomething();
 
-				if (!player->isAlive()) { //Checks if player dies and returns dies
-					return GWSTATUS_PLAYER_DIED;
-				}
-				if(completeLevel()){ // Checks if player completes level
-					return GWSTATUS_FINISHED_LEVEL;
-				}
-			}
-		}
+                if (!player->isAlive()) { //Checks if player dies and returns dies
+                    return GWSTATUS_PLAYER_DIED;
+                }
+                
+                if(completeLevel()){ // Checks if player completes level
+                    return GWSTATUS_FINISHED_LEVEL;
+                }
+                
+            }
+        }
 
-		//Removes all unalive objects are screen
-		removeDeadObjects();
+        //Removes all unalive objects are screen
+        removeDeadObjects();
 
-		//Asks if player is alive
-		if (!player->isAlive()) {
-			return GWSTATUS_PLAYER_DIED;
-		}
+        //Asks if player is alive
+        if (!player->isAlive()) {
+            return GWSTATUS_PLAYER_DIED;
+        }
 
         //if (false) {
         //    decLives();
         //}
 
-		//If the lives equal zero, then return the player has perished
+        //If the lives equal zero, then return the player has perished
         if (getLives() == 0) {
             return GWSTATUS_PLAYER_DIED;
         }
-		//Otherwise return continue game
+        //Otherwise return continue game
         return GWSTATUS_CONTINUE_GAME;
     }
     /*
@@ -152,46 +158,46 @@ public:
     */
     virtual void cleanUp()
     {
-		//Deletes all the stuff in icemap 
-		for (int i = 0; i <= 59; i++) {
-			for (int j = 0; j <= 64; j++) {
-				delete iceMap.at(i).at(j); //deletes every ice in vector;
-			}
-		}
-		//Clears the vector out
-		iceMap.clear();
+        //Deletes all the stuff in icemap
+        for (int i = 0; i <= 59; i++) {
+            for (int j = 0; j <= 64; j++) {
+                delete iceMap.at(i).at(j); //deletes every ice in vector;
+            }
+        }
+        //Clears the vector out
+        iceMap.clear();
 
-		//Delete all the actors in character list
-		for (Actor* a : characterList) {
-			Actor* temp = a;
-			a = nullptr;
-			delete temp;
-		}
-		characterList.clear();
-		//delete characterList;
+        //Delete all the actors in character list
+        for (Actor* a : characterList) {
+            Actor* temp = a;
+            a = nullptr;
+            delete temp;
+        }
+        characterList.clear();
+        //delete characterList;
     }
 
-	bool completeLevel() {
-		return false;//for now
-	}
+    bool completeLevel() {
+        return false;//for now
+    }
 
-	//Returns the characterlist
-	list<Actor*>& getCharacterList() {
-		return characterList;
-	}
+    //Returns the characterlist
+    list<Actor*>& getCharacterList() {
+        return characterList;
+    }
 
-	//Returns the map
-	vector<vector<Ice*>>& getMap() {
-		return iceMap;
-	}
+    //Returns the map
+    vector<vector<Ice*>>& getMap() {
+        return iceMap;
+    }
 
 private:
-	Actor* a;
-	vector<vector<Ice*>> iceMap; //Used to keep track of ice on map
-	list<Actor*> characterList;
-	IceMan* player;
+    Actor* a;
+    vector<vector<Ice*>> iceMap; //Used to keep track of ice on map
+    list<Actor*> characterList;
+    IceMan* player;
 
-	  void removeDeadObjects();
+      void removeDeadObjects();
 
     //Creates the icemap
     void createIceMap();
@@ -200,6 +206,9 @@ private:
 
     //Spawns the gold nuggets
     void spawnNuggets(int num);
+    
+    //Spawn Oil
+    void spawnOil(int oNum);
     //Checks the distance between 2 objects
     bool checkDistance(Actor* a, int obj1X, int obj1Y, int obj2X, int obj2Y);
 
